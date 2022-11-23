b69b4c5e4b8975c21e032a6bc0401698448aa652
linsourcecode <linzilong813@gmail.com>
2022-11-23 13:29:18 +0800

    完成用户地址的增删查功能

---
+++ b2c-user/src/main/java/org/example/user/controller/AddressController.java
@@ -0,0 +1,56 @@
+package org.example.user.controller;
+
+
+import org.example.parms.AddressListParam;
+import org.example.parms.AddressRemoveParam;
+import org.example.pojo.Address;
+import org.example.user.service.AddressService;
+
+import org.example.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+/**
+ * 提供用户地址增删查接口
+ * */
+@RestController
+@RequestMapping("user/Address")
+public class AddressController {
+    @Autowired
+    private AddressService addressService;
+    /**
+     * 提供用户地址查询接口
+     * */
+    @ResponseBody
+    @PostMapping("list")
+    public R list(@RequestBody @Validated AddressListParam addressListParam, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return R.fail("参数异常，查询失败");
+        }
+        return  addressService.list(addressListParam.getUserId());
+
+    }
+    /**
+     * 提供用户地址保存
+     * */
+    @ResponseBody
+    @PostMapping("save")
+    public R save(@RequestBody @Validated Address address, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return  R.fail("参数异常,失败");
+        }
+        return  addressService.save(address);
+    }
+    @ResponseBody
+    @PostMapping("remove")
+    public R remove(@RequestBody @Validated AddressRemoveParam addressRemoveParam, BindingResult result){
+
+        if (result.hasErrors()){
+
+            return R.fail("参数异常,删除失败!");
+        }
+
+        return addressService.remove(addressRemoveParam.getId());
+    }
+}
---
+++ b2c-user/src/main/java/org/example/user/service/impl/AddressServiceImpl.java
@@ -0,0 +1,68 @@
+package org.example.user.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.example.pojo.Address;
+import org.example.user.mapper.AddressMapper;
+import org.example.user.service.AddressService;
+import org.example.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+/**
+ * 此类提供用户地址服务方法
+ * */
+@Slf4j
+@Service
+public class AddressServiceImpl implements AddressService {
+    @Autowired
+    private AddressMapper addressMapper;
+    /**提供用户地址查询服务
+     * */
+    @Override
+    public R list(Integer userId) {
+        //1,封装查询参数
+        QueryWrapper<Address> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id",userId);
+        List<Address> addressList = addressMapper.selectList(queryWrapper);
+        //2.结果封装
+        R ok = R.ok("查询成功", addressList);
+
+
+
+        return ok;
+    }
+    /**提供用户地址保存服务
+     * */
+    @Override
+    public R save(Address address) {
+        log.info("详细信息为 "+address);
+        int rows = addressMapper.insert(address);
+        //2.插入成功
+        if (rows == 0){
+            log.info("AddressServiceImpl.save业务结束，结果:{}","地址失败!");
+            return R.fail("插入地址失败!");
+        }
+        //复用查询业务
+        return list(address.getUserId());
+    }
+    /**
+     *
+     * 根据id 删除用户地址数据
+     */
+    @Override
+    public R remove(Integer id) {
+
+        int rows = addressMapper.deleteById(id);
+
+        if (rows == 0){
+            log.info("AddressServiceImpl.remove业务结束，结果:{}","地址删除失败");
+            return R.fail("删除地址数据失败!");
+        }
+
+        log.info("AddressServiceImpl.remove业务结束，结果:{}","地址删除成功!");
+
+        return R.ok("地址删除成功!");
+    }
+}
